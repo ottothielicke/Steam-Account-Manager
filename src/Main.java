3ac3422759f23d72a3a73486246f761c594f7b06
ottothielicke <44933123+ottothielicke@users.noreply.github.com>
2019-05-29 22:22:29 -0700

    gitignore class files

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,12 @@ public class Main {
     private static AccountHandler accountHandler;
     public static void main(String[] args){
         InputStream configURL = Main.class.getResourceAsStream("configuration.txt");
-
         Configuration config = new Configuration(configURL);
         accountHandler = new AccountHandler(config.getAccountFileLocation());
         SwingUtilities.invokeLater(() -> {
             window = new MainWindow();
             window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             window.setVisible(true);
-
             PrintStream psOut = new PrintStream(new OutputStreamHandler(window.getConsoleArea()));
             PrintStream psErr = new PrintStream(new OutputStreamHandler(window.getConsoleArea()));
             System.setOut(psOut);
